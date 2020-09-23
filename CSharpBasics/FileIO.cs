6870fbbe96304ae29bae850382ecd4d611d52b4a
Sushanghimire-151 <sushanghimire151@gmail.com>
2020-09-23 18:38:50 +0545

    Learned about simple file/directory IO

---
+++ CSharpBasics/FileIO.cs
@@ -0,0 +1,38 @@
+using System;
+using System.IO;
+
+public class FileIO
+{
+    public void LearnFileHandling()
+    {
+        //string fileContent = File.ReadAllText(@"D:\sixth sem\NCC\Lab\Sushan.ghimire\Readme.txt"); //@ is used to remove escape sequence. Can also use double slash(//)
+        string fileName = "file.txt";
+        if (File.Exists(fileName))
+        {
+            string fileContent = File.ReadAllText("Iterion.cs");
+            Console.WriteLine(fileContent);
+        }
+
+        File.WriteAllText(fileName, "This is just a text file");
+    }
+
+    public void LearnFileInfo()
+    {
+        string fileName = "file.txt";
+        FileInfo fileInfo = new FileInfo(fileName);
+        var size = fileInfo.Length;
+        var createdDate = fileInfo.CreationTime;
+
+        Console.WriteLine($"File size: {size} bytes");
+        Console.WriteLine($"Created Date: {createdDate}");
+    }
+
+    public void LearnDirectories()
+    {
+        string directoryName = "A";
+        Directory.CreateDirectory(directoryName); // makes a new directory
+    }
+
+    // Ques: Create a folder "ABC", create 10 sub folders in "ABC". Each subfolder
+    // should contain a C# file with "Hello World" program.
+}
--- CSharpBasics/Program.cs
+++ CSharpBasics/Program.cs
@@ -5,16 +5,23 @@ public class Program
 {
     static void Main()
     {
-        // // Main program for collection 
+        // // Main program for collection:
         // Collections collections = new Collections();
         // collections.LearnDictionary();
 
-        // Main program for Inheritence:
-        var square = new Square(5.5);
-        square.Display();
-        square.GetDiagonal();
+
+
+        // // Main program for Inheritence:
+        // var square = new Square(5.5);
+        // square.Display();
+        // square.GetDiagonal();
         
-        var rectangle = new Rectangle(5.5, 2.5);
-        rectangle.Display();
+        // var rectangle = new Rectangle(5.5, 2.5);
+        // rectangle.Display();
+
+
+        // Main program for FileIO:
+        FileIO fileIO = new FileIO();
+        fileIO.LearnDirectories();
     }
 }
