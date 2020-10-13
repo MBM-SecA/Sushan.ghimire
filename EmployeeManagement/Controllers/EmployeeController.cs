cf879aec8e4c4790040b03e42f41b57ef0c57feb
Sushanghimire-151 <sushanghimire151@gmail.com>
2020-10-13 10:14:32 +0545

    Employee detail view and controller updated with

--- EmployeeManagement/Controllers/EmployeeController.cs
+++ EmployeeManagement/Controllers/EmployeeController.cs
@@ -5,21 +5,23 @@ public class EmployeeController : Controller
 {
     public ActionResult Index()
     {
-
-
-        //Object Initializer Syntax
-        Person emp1 = new Person() { FirstName = "Sushan", Surname = "Ghimire", Address = "Lagankhel", Salary = 20000.0 };
-        Person emp2 = new Person() { FirstName = "Ram", Surname = "Bahadur", Address = "Bhaktapur", Salary = 20000.0 };
-        Person emp3 = new Person() { FirstName = "Ram", Surname = "Bahadur", Address = "Bhaktapur", Salary = 20000.0 };
-        Person emp4 = new Person() { FirstName = "Ram", Surname = "Bahadur", Address = "Bhaktapur", Salary = 20000.0 };
-        Person emp5 = new Person() { FirstName = "Ram", Surname = "Bahadur", Address = "Bhaktapur", Salary = 20000.0 };
-        Person emp6 = new Person() { FirstName = "Ram", Surname = "Bahadur", Address = "Bhaktapur", Salary = 20000.0 };
-        Person emp7 = new Person() { FirstName = "Ram", Surname = "Bahadur", Address = "Bhaktapur", Salary = 20000.0 };
-        Person emp8 = new Person() { FirstName = "Ram", Surname = "Bahadur", Address = "Bhaktapur", Salary = 20000.0 };
-
-        List<Person> employees = new List<Person>() { emp1, emp2, emp3, emp4, emp5, emp6, emp7, emp8};
+        List<Person> employees = Person.GetPerson();
 
         return View(employees);
+    }
+
+    public ActionResult Detail(string firstName)
+    {
+        List<Person> employees = Person.GetPerson();
+        //Person p = null;
+        foreach(var employee in employees)
+        {
+            if(employee.FirstName == firstName)
+            {
+                return View(employee);
+            }
+        }
+        return View();
     }
 }
 
--- EmployeeManagement/Models/Person.cs
+++ EmployeeManagement/Models/Person.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+
 public class Person
 {
     public string FirstName { get; set; }
@@ -5,5 +7,21 @@ public class Person
     public string Address { get; set; }
     public char Gender { get; set; } = 'M';
     public double Salary { get; set; }
+
+    public static List<Person> GetPerson()
+    {
+        //Object Initializer Syntax
+        Person emp1 = new Person() { FirstName = "Sushan", Surname = "Ghimire", Address = "Lagankhel", Salary = 20000.0 };
+        Person emp2 = new Person() { FirstName = "Ram", Surname = "Bahadur", Address = "Bhaktapur", Salary = 20000.0 };
+        Person emp3 = new Person() { FirstName = "Shyam", Surname = "Bahadur", Address = "Bhaktapur", Salary = 20000.0 };
+        Person emp4 = new Person() { FirstName = "Rita", Surname = "Bahadur", Address = "Bhaktapur", Salary = 20000.0 };
+        Person emp5 = new Person() { FirstName = "Rikesh", Surname = "Bahadur", Address = "Bhaktapur", Salary = 20000.0 };
+        Person emp6 = new Person() { FirstName = "Jame", Surname = "Bahadur", Address = "Bhaktapur", Salary = 20000.0 };
+        Person emp7 = new Person() { FirstName = "Pulu", Surname = "Bahadur", Address = "Bhaktapur", Salary = 20000.0 };
+        Person emp8 = new Person() { FirstName = "Lala", Surname = "Bahadur", Address = "Bhaktapur", Salary = 20000.0 };
+
+        return new List<Person> { emp1, emp2, emp3, emp4, emp5, emp6, emp7, emp8 };
+
+    }
 
 }
