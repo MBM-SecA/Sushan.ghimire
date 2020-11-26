fec55dc08fc83d462144762154f61d9fd63ed796
Sushanghimire-151 <sushanghimire151@gmail.com>
2020-11-26 10:41:47 +0545

    added details and edit page

--- EmployeeManagement/Controllers/EmployeeController.cs
+++ EmployeeManagement/Controllers/EmployeeController.cs
@@ -1,27 +1,32 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 
 public class EmployeeController : Controller
 {
     public ActionResult Index()
     {
-        List<Person> employees = Person.GetPerson();
-
+        List<Person> employees = Person.GetEmployees();
         return View(employees);
     }
 
-    public ActionResult Detail(string firstName)
+    public ActionResult Detail([FromQuery] int id)
     {
-        List<Person> employees = Person.GetPerson();
-        //Person p = null;
-        foreach(var employee in employees)
-        {
-            if(employee.FirstName == firstName)
-            {
-                return View(employee);
-            }
-        }
+        List<Person> employees = Person.GetEmployees();
+        var employee = employees.FirstOrDefault(x => x.Id == id);
+        return View(employee);
+    }
+
+    public ActionResult Add()
+    {
         return View();
+    }
+
+    [HttpPost]
+    public ActionResult<string> Add([FromForm] Person person)
+    {
+        return "Record Saved";
     }
 }
 
--- EmployeeManagement/Models/Person.cs
+++ EmployeeManagement/Models/Person.cs
@@ -1,26 +1,36 @@
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 public class Person
 {
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "I need a first name")]
+    [Display(Name = "First Name")]
     public string FirstName { get; set; }
+
+    [Required, Display(Name = "Last Name")]
+
     public string Surname { get; set; }
     public string Address { get; set; }
-    public char Gender { get; set; } = 'M';
-    public double Salary { get; set; }
+    public char? Gender { get; set; } = 'M';
+    public double? Salary { get; set; }
 
-    public static List<Person> GetPerson()
+    public static List<Person> GetEmployees()
     {
         //Object Initializer Syntax
-        Person emp1 = new Person() { FirstName = "Sushan", Surname = "Ghimire", Address = "Lagankhel", Salary = 20000.0 };
-        Person emp2 = new Person() { FirstName = "Ram", Surname = "Bahadur", Address = "Bhaktapur", Salary = 20000.0 };
-        Person emp3 = new Person() { FirstName = "Shyam", Surname = "Bahadur", Address = "Bhaktapur", Salary = 20000.0 };
-        Person emp4 = new Person() { FirstName = "Rita", Surname = "Bahadur", Address = "Bhaktapur", Salary = 20000.0 };
-        Person emp5 = new Person() { FirstName = "Rikesh", Surname = "Bahadur", Address = "Bhaktapur", Salary = 20000.0 };
-        Person emp6 = new Person() { FirstName = "Jame", Surname = "Bahadur", Address = "Bhaktapur", Salary = 20000.0 };
-        Person emp7 = new Person() { FirstName = "Pulu", Surname = "Bahadur", Address = "Bhaktapur", Salary = 20000.0 };
-        Person emp8 = new Person() { FirstName = "Lala", Surname = "Bahadur", Address = "Bhaktapur", Salary = 20000.0 };
+        Person emp1 = new Person() { Id = 1 ,FirstName = "Sushan", Surname = "Ghimire", Address = "Lagankhel", Salary = 20000.0 };
+        Person emp2 = new Person() { Id = 2 ,FirstName = "Ram", Surname = "Bahadur", Address = "Bhaktapur", Salary = 20000.0 };
+        Person emp3 = new Person() { Id = 3 ,FirstName = "Shyam", Surname = "Bahadur", Address = "Bhaktapur", Salary = 20000.0 };
+        Person emp4 = new Person() { Id = 4 ,FirstName = "Rita", Surname = "Bahadur", Address = "Bhaktapur", Salary = 20000.0 };
+        Person emp5 = new Person() { Id = 5 ,FirstName = "Rikesh", Surname = "Bahadur", Address = "Bhaktapur", Salary = 20000.0 };
+        Person emp6 = new Person() { Id = 6 ,FirstName = "Jame", Surname = "Bahadur", Address = "Bhaktapur", Salary = 20000.0 };
+        Person emp7 = new Person() { Id = 7 ,FirstName = "Pulu", Surname = "Bahadur", Address = "Bhaktapur", Salary = 20000.0 };
+        Person emp8 = new Person() { Id = 8 ,FirstName = "Lala", Surname = "Bahadur", Address = "Bhaktapur", Salary = 20000.0 };
+        Person emp9 = new Person() { Id = 9 ,FirstName = "Lalita", Surname = "Kumari", Address = "Kirtipur", Salary = 20000.0, Gender = 'F' };
 
-        return new List<Person> { emp1, emp2, emp3, emp4, emp5, emp6, emp7, emp8 };
+        return new List<Person> { emp1, emp2, emp3, emp4, emp5, emp6, emp7, emp8, emp9 };
 
     }
 
