9de795d45a1b221f934b51ec68a7331209b9bd1a
Sushanghimire-151 <sushanghimire151@gmail.com>
2020-12-09 15:54:19 +0545

    Employee CRUD operation completed

--- EmployeeManagement/Controllers/EmployeeController.cs
+++ EmployeeManagement/Controllers/EmployeeController.cs
@@ -18,23 +18,56 @@ public class EmployeeController : Controller
 
     public ActionResult Detail([FromQuery] int id)
     {
-        List<Person> employees = Person.GetEmployees();
-        var employee = employees.FirstOrDefault(x => x.Id == id);
+        var employee = db.People.Find(id);
+
         return View(employee);
     }
 
+    [HttpGet]
     public ActionResult Add()
     {
         return View();
     }
 
     [HttpPost]
-    public ActionResult<string> Add([FromForm] Person person)
+    public ActionResult Add(Person person)
     {
         db.People.Add(person);
         db.SaveChanges();
-        
-        return "Record Saved";
+
+        return RedirectToAction(nameof(Index));
+    }
+
+    public ActionResult Edit(int id)
+    {
+        var employee = db.People.Find(id);
+        return View(employee);
+    }
+
+    [HttpPost]
+    public ActionResult Edit(Person person)
+    {
+        db.People.Attach(person);
+        db.People.Update(person);
+        db.SaveChanges();
+
+        return RedirectToAction(nameof(Index));
+    }
+
+    public ActionResult Delete(int id)
+    {
+        var employee = db.People.Find(id);
+        return View(employee);
+    }
+
+    [HttpPost]
+    public ActionResult Delete(Person person)
+    {
+        db.People.Attach(person);
+        db.People.Remove(person);
+        db.SaveChanges();
+        return RedirectToAction(nameof(Index));
     }
 }
+
 
